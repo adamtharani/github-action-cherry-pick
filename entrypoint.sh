2a19d9241197ffcb95fed6cb762157cac1ec66e1
adamtharani <adamtharani@me.com>
2021-01-08 15:17:56 -0500

    Allow cherry-picking merge commits

--- entrypoint.sh
+++ entrypoint.sh
@@ -31,12 +31,19 @@ if [[ $MESSAGE -gt 0 ]]; then
   exit 0
 fi
 
+MERGE_COMMIT=$(git log -1 $GITHUB_SHA | grep "Merge pull request" | wc -l | xargs)
+ALLOW_MERGES=""
+
+if [[ $MERGE_COMMIT -eq 1 ]]; then
+  ALLOW_MERGES="-m 1"
+fi
+
 PR_TITLE=$(git log -1 --format="%s" $GITHUB_SHA)
 
 git_setup
 git_cmd git remote update
 git_cmd git fetch --all
 git_cmd git checkout -b "${PR_BRANCH}" origin/"${INPUT_PR_BRANCH}"
-git_cmd git cherry-pick "${GITHUB_SHA}"
+git_cmd git cherry-pick ${ALLOW_MERGES} "${GITHUB_SHA}"
 git_cmd git push -u origin "${PR_BRANCH}"
 git_cmd hub pull-request -b "${INPUT_PR_BRANCH}" -h "${PR_BRANCH}" -l "${INPUT_PR_LABELS}" -a "${GITHUB_ACTOR}" -m "\"AUTO: ${PR_TITLE}\""
